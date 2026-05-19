6b0586ff26d8d2507158af72e35a16b01a213aa1
Theoe <theo.eco@outlook.fr>
2026-05-19 23:58:05 +0200

    add vscode extensions for opengl

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -3,5 +3,12 @@
 {
   programs.vscode = {
     enable = true;
+
+    extensions = with pkgs.vscode-extensions; [
+      ms-vscode.cpptools
+      ms-vscode.cmake-tools
+      twxs.cmake
+      jnoortheen.nix-ide
+    ];
   };
 }
