4d8240ccde1fdf038610fabe63759a7f7b8ea7cc
Theoe <theo.eco@outlook.fr>
2026-05-21 21:40:04 +0200

    improve vs code config

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -5,10 +5,31 @@
     enable = true;
 
     extensions = with pkgs.vscode-extensions; [
-      ms-vscode.cpptools
       ms-vscode.cmake-tools
       twxs.cmake
       jnoortheen.nix-ide
+      llvm-vs-code-extensions.vscode-clangd
     ];
+
+    userSettings = {
+      "workbench.iconTheme" = "material-icon-theme";
+
+      "files.associations" = {
+        "*.glsl" = "glsl";
+        "*.vert" = "glsl";
+        "*.frag" = "glsl";
+      };
+
+      "C_Cpp.intelliSenseEngine" = "disabled";
+      "clangd.arguments" = [
+        "--compile-commands-dir=build"
+      ];
+
+      "cmake.showConfigureWithDebuggerNotification" = false;
+      "explorer.confirmDragAndDrop" = false;
+      "explorer.confirmDelete" = false;
+
+      "editor.inlayHints.enabled" = "on";
+    };
   };
 }
